4f8b00a7880fabb8440e02b1a12f45f8a4c7b3f5
Angel Aguilar Armendariz <70615645+aaguilarar@users.noreply.github.com>
2020-11-11 07:51:33 -0500

    Update uBPTreeEngine.pas

--- uBPTreeEngine.pas
+++ uBPTreeEngine.pas
@@ -2,20 +2,34 @@ unit uBPTreeEngine;
 
 interface
 
-uses Winapi.Windows, System.SysUtils;
+uses Winapi.Windows, System.SysUtils, System.Generics.Collections;
 
 type
-  cNodo = class
+  cNodo = class abstract
+  private
+    iListaLlaves: TList<Integer>;
+  public
+    function fnGetValorPorLlave(iLlave: Integer): String; Virtual; Abstract;
+    procedure fnEliminarValorPorLlave(iLlave: Integer); Virtual; Abstract;
+    procedure fnInsertarLlaveValor(iLlave: Integer; sValor: String); Virtual; Abstract;
+  end;
+
+  cNodoHoja = class(cNodo)
   private
     { Private declarations }
   public
-    { Public declarations }
+    sListaValores: TList<String>;
+    constructor fnCreate();
+    function fnGetValorPorLlave(iLlave: Integer): String; override;
+    procedure fnEliminarValorPorLlave(iLlave: Integer); override;
+    procedure fnInsertarLlaveValor(iLlave: Integer; sValor: String); override;
   end;
 
   cArbolBPlus = class
   private
     { Private declarations }
   public
+    nRaiz: cNodo;
     constructor fnCreate(iOrdenArbol: Integer);
     procedure fnInsertar(iLlave: Integer; sValor: String);
     procedure fnEliminar(iLlave: Integer);
@@ -23,12 +37,16 @@ type
     function fnImprimir(): string;
   end;
 
+var
+  iOrden: Integer = 3;
+
 implementation
 
 { cArbolBPlus }
 
 constructor cArbolBPlus.fnCreate(iOrdenArbol: Integer);
 begin
+  iOrden := iOrdenArbol;
 
 end;
 
@@ -39,7 +57,7 @@ end;
 
 procedure cArbolBPlus.fnInsertar(iLlave: Integer; sValor: String);
 begin
-
+  nRaiz.fnInsertarLlaveValor(iLlave, sValor);
 end;
 
 function cArbolBPlus.fnBuscar(iLlave: Integer): string;
@@ -49,6 +67,41 @@ end;
 
 function cArbolBPlus.fnImprimir(): string;
 begin
+
+end;
+
+{ cNodoHoja }
+
+constructor cNodoHoja.fnCreate;
+begin
+  iListaLlaves := TList<Integer>.Create();
+  sListaValores := TList<String>.Create();
+end;
+
+procedure cNodoHoja.fnEliminarValorPorLlave(iLlave: Integer);
+begin
+
+end;
+
+function cNodoHoja.fnGetValorPorLlave(iLlave: Integer): String;
+begin
+
+end;
+
+procedure cNodoHoja.fnInsertarLlaveValor(iLlave: Integer; sValor: String);
+var
+  iIndiceBuscado: Integer;
+  bEncontrado: Boolean;
+begin
+  bEncontrado := iListaLlaves.BinarySearch(iLlave, iIndiceBuscado);
+
+  if bEncontrado then
+    sListaValores[iIndiceBuscado] := sValor
+  else
+  begin
+    iListaLlaves.Insert(iIndiceBuscado, iLlave);
+    sListaValores.Insert(iIndiceBuscado, sValor);
+  end;
 
 end;
 
